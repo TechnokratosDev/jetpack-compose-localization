daafa55e3da83209df875c2ede9340050293a12c
VladislavTitov <titovvlad1997@yandex.ru>
2021-03-13 16:21:05 +0300

    remove PluralRulesHost & small fix

--- localization/src/main/java/com/technokratos/compose/localization/plurals.kt
+++ localization/src/main/java/com/technokratos/compose/localization/plurals.kt
@@ -28,8 +28,11 @@ fun Plurals(
 ): Localization.(Double) -> CharSequence {
     defaultLocalization.plurals[name] = defaultValue
     for ((locale, value) in localeToPlural().entries) {
-        if (PluralRulesHost.checkLocale(locale)) {
-            Log.w("jcl10n", "There is no plural rule for $locale currently. Plural's 'other = ${value.other}' field can be used only!")
+        if (!isLocaleRegistered(locale)) {
+            Log.w(
+                "jcl10n",
+                "There is no plural rule for $locale currently. Plural's 'other = ${value.other}' field can be used only!"
+            )
         }
         val localization =
             localizationMap[locale] ?: throw RuntimeException("There is no locale $locale")
@@ -48,44 +51,58 @@ fun Localization.getPlural(name: String, quantity: Double): CharSequence? {
     val fractionPart = frac.trimStart('0').ifEmpty { "0" }.toLong()
     val fractionPartDigitCount = frac.trimEnd('0').count()
 
-    return plurals[name]?.let { PluralRulesHost.resolveString(it, locale, absQuantity, integerPart, fractionPart, fractionPartDigitCount) }
+    return plurals[name]?.let {
+        resolveString(
+            it,
+            locale,
+            absQuantity,
+            integerPart,
+            fractionPart,
+            fractionPartDigitCount
+        )
+    }
 }
 
-private object PluralRulesHost {
-    private val defaultPluralRule = PluralRule()
-    private val localeToPluralRule: Map<Locale, PluralRule> = mutableMapOf(
-        Locale.ENGLISH to PluralRule(
-            one = { _: Double, i: Long, _: Long, v: Int ->
-                i == 1L && v == 0
-            }
-        ),
-        Locale("ru") to PluralRule(
-            one = { _: Double, i: Long, _: Long, v: Int ->
-                v == 0 && i % 10 == 1L && i % 100 != 11L
-            },
-            few = { _: Double, i: Long, _: Long, v: Int ->
-                v == 0 && i % 10 in 2..4 && i % 100 !in 12..14
-            },
-            many = { _: Double, i: Long, _: Long, v: Int ->
-                v == 0 && (i % 10 == 0L || i % 10 in 5..9 || i % 100 in 11..14)
-            }
-        )
-    )
-
-    fun checkLocale(locale: Locale): Boolean {
-        return localeToPluralRule.containsKey(locale)
-    }
-
-    fun resolveString(plural: Plural, locale: Locale, n: Double, i: Long, f: Long, v: Int): CharSequence {
-        val rule = localeToPluralRule[locale] ?: defaultPluralRule
-
-        return when {
-            rule.zero(n, i, f, v) && plural.zero != null -> plural.zero
-            rule.one(n, i, f, v) && plural.one != null -> plural.one
-            rule.two(n, i, f, v) && plural.two != null -> plural.two
-            rule.few(n, i, f, v) && plural.few != null -> plural.few
-            rule.many(n, i, f, v) && plural.many != null -> plural.many
-            else -> plural.other
+private val defaultPluralRule = PluralRule()
+private val localeToPluralRule: Map<Locale, PluralRule> = mutableMapOf(
+    Locale.ENGLISH to PluralRule(
+        one = { _: Double, i: Long, _: Long, v: Int ->
+            i == 1L && v == 0
         }
+    ),
+    Locale("ru") to PluralRule(
+        one = { _: Double, i: Long, _: Long, v: Int ->
+            v == 0 && i % 10 == 1L && i % 100 != 11L
+        },
+        few = { _: Double, i: Long, _: Long, v: Int ->
+            v == 0 && i % 10 in 2..4 && i % 100 !in 12..14
+        },
+        many = { _: Double, i: Long, _: Long, v: Int ->
+            v == 0 && (i % 10 == 0L || i % 10 in 5..9 || i % 100 in 11..14)
+        }
+    )
+)
+
+fun isLocaleRegistered(locale: Locale): Boolean {
+    return localeToPluralRule.containsKey(locale)
+}
+
+fun resolveString(
+    plural: Plural,
+    locale: Locale,
+    n: Double,
+    i: Long,
+    f: Long,
+    v: Int
+): CharSequence {
+    val rule = localeToPluralRule[locale] ?: defaultPluralRule
+
+    return when {
+        rule.zero(n, i, f, v) && plural.zero != null -> plural.zero
+        rule.one(n, i, f, v) && plural.one != null -> plural.one
+        rule.two(n, i, f, v) && plural.two != null -> plural.two
+        rule.few(n, i, f, v) && plural.few != null -> plural.few
+        rule.many(n, i, f, v) && plural.many != null -> plural.many
+        else -> plural.other
     }
 }
